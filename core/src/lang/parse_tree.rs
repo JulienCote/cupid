4e3bf8aade8589d52ae3ddccf0291f9b4b6faebe
Julien Côté <julien.jc.cote@gmail.com>
2025-12-14 16:05:25 -0500

    slight restructure of ParseTree

--- core/src/lang/parse_tree.rs
+++ core/src/lang/parse_tree.rs
@@ -3,11 +3,11 @@ use crate::lang::SuperType;
 /// defines an expression but isn't evaluated.
 /// This is the upmost structure representing code before being compiled into instructions.
 pub struct ParseTree {
-    nodes: Vec<ParseTreeNode>,
+    //TODO: invokable: Invokable,
+    arguments: Vec<ParseTreeNode>,
 }
 
 pub enum ParseTreeNode {
-    Invokable(/* TODO */),
     Literal(SuperType),
     Variable(String),
     ParseTree(ParseTree),
